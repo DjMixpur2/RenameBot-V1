c67d51fe2a0452c014a1e4dbf938c9149da6ba8c
DjMixpur2 <djmixpur2@gmail.com>
2025-08-11 23:12:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from decouple import config
 
 # Load configurations using decouple
-API_ID = config('API_ID', cast=int)  # Cast to int if the value is an integer
-API_HASH = config('API_HASH')        # No cast needed for string values
-BOT_TOKEN = config('BOT_TOKEN')
+API_ID = config('API_ID', "21344245")  # Cast to int if the value is an integer
+API_HASH = config('API_HASH', "e72de40c666f1664f847a79f97dd3882")        # No cast needed for string values
+BOT_TOKEN = config('BOT_TOKEN', "7082591322:AAGQm5X0B5pvrT0AeNfe3KUB_cQWOq3zp5c")
 # Convert the ADMIN field from the .env into a list of integers
-ADMIN = [int(admin_id) for admin_id in config('ADMIN', default='').split(',') if admin_id]
+ADMIN = [int(admin_id) for admin_id in config('ADMIN', "7585839477").split(',') if admin_id]
 CAPTION = config('CAPTION', default="")  # Optional field, set a default value if missing
 
 # Set download location for media (can be changed to a configurable value)
